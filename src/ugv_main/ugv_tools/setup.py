8b32522078a8ee919b994434a75f600eec864101
Rick van der Veen <rickvdv104@gmail.com>
2025-11-05 14:10:07 +0100

    Fix missing comma

--- src/ugv_main/ugv_tools/setup.py
+++ src/ugv_main/ugv_tools/setup.py
@@ -25,7 +25,7 @@ setup(
         'console_scripts': [
             'keyboard_ctrl = ugv_tools.keyboard_ctrl:main',
             'joy_ctrl = ugv_tools.joy_ctrl:main',
-            'behavior_ctrl = ugv_tools.behavior_ctrl:main'
+            'behavior_ctrl = ugv_tools.behavior_ctrl:main',
             'view_lines = ugv_tools.view_lines:main',
             'rerun_logging = ugv_tools.rerun_logging:main',
         ],
