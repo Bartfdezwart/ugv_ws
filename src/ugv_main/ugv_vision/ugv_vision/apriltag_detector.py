5940f89a4e1f40a5ded34dcd1c51f051d6ddf0c2
KT-47 <brouwer427@outlook.com>
2025-11-13 14:45:24 +0100

    Created first prototype of distance estimation

--- src/ugv_main/ugv_vision/ugv_vision/apriltag_detector.py
+++ src/ugv_main/ugv_vision/ugv_vision/apriltag_detector.py
@@ -14,8 +14,8 @@ class ApriltagCtrl(Node):
     def __init__(self):
         super().__init__('apriltag_ctrl')
         # Create a subscription to the image_raw topic
-        self.image_raw_subscription = self.create_subscription(Image, '/image_raw', self.image_callback,10)
-        # self.image_raw_subscription = self.create_subscription(CompressedImage, '/image_rect/compressed', self.image_callback,10)
+        # self.image_raw_subscription = self.create_subscription(Image, '/image_raw', self.image_callback,10)
+        self.image_raw_subscription = self.create_subscription(CompressedImage, '/image_rect/compressed', self.image_callback,10)
         # Create a publisher to the apriltag_ctrl/result topic
         self.apriltag_ctrl_publisher = self.create_publisher(Image, '/apriltag_ctrl/result', 10)
         # Create a CvBridge object to convert between ROS Image messages and OpenCV images
@@ -24,6 +24,23 @@ class ApriltagCtrl(Node):
         # self.detector = apriltag("tag36h11")
         self.detector = apriltag("tagStandard41h12")
         
+        # self.focal_length = 554.256  # Focal length in pixels
+        # self.focal_length = 413.6420  # Focal length in pixels
+        self.focal_length = 289.43385   # Focal length in pixels
+        
+        # self.tag_width = 0.158  # Tag width in meters
+        # self.tag_width = 0.165  # Tag width in meters
+        self.tag_width = 0.58  # Small tag width in meters
+        
+        self.intrinsic_params = np.array([[289.11451,   0.     , 347.23664],
+                                          [0.     , 289.75319, 235.67429],
+                                          [0.     ,   0.     ,   1.     ]])
+        
+        self.tag_points_3d = np.array([[-self.tag_width/2, -self.tag_width/2, 0],
+                                       [ self.tag_width/2, -self.tag_width/2, 0],
+                                       [ self.tag_width/2,  self.tag_width/2, 0],
+                                       [-self.tag_width/2,  self.tag_width/2, 0]])
+        
     def detect_apritag(self, frame):
 
         return type
@@ -31,13 +48,13 @@ class ApriltagCtrl(Node):
     def image_callback(self, msg):
 
         # Convert the ROS Image message to an OpenCV image
-        frame = self.bridge.imgmsg_to_cv2(msg, "bgr8")
-        # frame = self.bridge.compressed_imgmsg_to_cv2(msg, "bgr8")
+        # frame = self.bridge.imgmsg_to_cv2(msg, "bgr8")
+        frame = self.bridge.compressed_imgmsg_to_cv2(msg, "bgr8")
 
         # Convert the image to grayscale
         scale = 2
+        frame = cv2.resize(frame, (frame.shape[1] * scale, frame.shape[0] * scale))
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        gray = cv2.resize(gray, (gray.shape[1] * scale, gray.shape[0] * scale))
         
         # Apply histogram equalization
         # gray = cv2.equalizeHist(gray)
@@ -55,7 +72,7 @@ class ApriltagCtrl(Node):
         # Detect apriltags in the image
         results = self.detector.detect(gray)
         
-        frame = cv2.cvtColor(gray, cv2.COLOR_GRAY2BGR)
+        # frame = cv2.cvtColor(gray, cv2.COLOR_GRAY2BGR)
 
         # Loop through the detected apriltags
         for r in results:
@@ -69,9 +86,25 @@ class ApriltagCtrl(Node):
             center_x, center_y = int(r['center'][0]), int(r['center'][1])
             # Draw a circle at the center of the apriltag
             cv2.circle(frame, (center_x, center_y), 5, (0, 0, 255), -1)
+            
+            width_in_img = np.linalg.norm(corners[0] - corners[1]) + np.linalg.norm(corners[0] - corners[1]) / 2
+            detect_distance = (self.tag_width * self.focal_length) / width_in_img
+            # detect_distance += 0.11  # Current error margin calibration to camera lens
+            detect_distance /= 5
+            detect_distance -= 0.02  # Current error margin calibration to camera lens
+            
+            # rvec, tvec, _ = cv2.solvePnP(self.tag_points_3d,
+            #                              r['lb-rb-rt-lt'].astype(np.float32),
+            #                              self.intrinsic_params,
+            #                              None)
+            
+            # print(tvec)
+            # detect_distance = tvec[2][0]
         
             # Print the ID and center of the apriltag
             print(f'Tag ID: {r["id"]}, Center: ({center_x}, {center_y})')
+            print(f'Estimated distance: {detect_distance:.2f} meters')
+            # print(r)
 
         # Convert the OpenCV image back to a ROS Image message
         frame = cv2.resize(frame, (frame.shape[1] // scale, frame.shape[0] // scale))
