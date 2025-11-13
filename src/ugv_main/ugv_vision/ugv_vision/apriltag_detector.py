6afc66f7df51eeb2bb52d9c09d616b5394498e1a
Bart de Zwart <bart.de.zwart@student.uva.nl>
2025-11-13 18:26:16 +0100

    added alternative preprocessing for testing

--- src/ugv_main/ugv_vision/ugv_vision/apriltag_detector.py
+++ src/ugv_main/ugv_vision/ugv_vision/apriltag_detector.py
@@ -1,7 +1,6 @@
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image
-from sensor_msgs.msg import CompressedImage
 from std_msgs.msg import String
 from cv_bridge import CvBridge
 
@@ -12,10 +11,9 @@ from apriltag import apriltag
 
 class ApriltagCtrl(Node):
     def __init__(self):
-        super().__init__('apriltag_ctrl')
+        super().__init__('apriltag_detector')
         # Create a subscription to the image_raw topic
-        self.image_raw_subscription = self.create_subscription(Image, '/image_raw', self.image_callback,10)
-        # self.image_raw_subscription = self.create_subscription(CompressedImage, '/image_rect/compressed', self.image_callback,10)
+        self.image_raw_subscription = self.create_subscription(Image,'/image_raw', self.image_callback,10)
         # Create a publisher to the apriltag_ctrl/result topic
         self.apriltag_ctrl_publisher = self.create_publisher(Image, '/apriltag_ctrl/result', 10)
         # Create a CvBridge object to convert between ROS Image messages and OpenCV images
@@ -32,30 +30,37 @@ class ApriltagCtrl(Node):
 
         # Convert the ROS Image message to an OpenCV image
         frame = self.bridge.imgmsg_to_cv2(msg, "bgr8")
-        # frame = self.bridge.compressed_imgmsg_to_cv2(msg, "bgr8")
+
+        scale_percent = 300 # percent of original size
+        width = int(frame.shape[1] * scale_percent / 100)
+        height = int(frame.shape[0] * scale_percent / 100)
+        dim = (width, height)
+        
+        # resize image
+        resized = cv2.resize(frame, dim, interpolation = cv2.INTER_AREA)
+
+        contrast = 0.3
+        resized = cv2.addWeighted(resized, contrast, np.zeros(resized.shape, resized.dtype), 0, 0)
 
         # Convert the image to grayscale
-        scale = 2
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        gray = cv2.resize(gray, (gray.shape[1] * scale, gray.shape[0] * scale))
-        
-        # Apply histogram equalization
-        # gray = cv2.equalizeHist(gray)
+        gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
 
-        # Apply CLAHE (Contrast Limited Adaptive Histogram Equalization)
-        clahe = cv2.createCLAHE(clipLimit=40, tileGridSize=(4,4))
+        clahe = cv2.createCLAHE(clipLimit=80, tileGridSize=(2,2))
+        # gray = np.clip(clahe.apply(gray) + 30, 0, 255).astype(np.uint8)
         gray = clahe.apply(gray)
-        
-        # Sharpen image
-        kernel = np.array([[0, -1, 0],
-                           [-1, 5,-1],
-                           [0, -1, 0]])
-        gray = cv2.filter2D(gray, -1, kernel)
+
+        # Create the sharpening kernel
+        kernel = np.array([[0, -2, 0], [-2, 9, -2], [0, -2, 0]])
+        # Sharpen the image
+        sharpened_image = cv2.filter2D(gray, -1, kernel)
+
+
+
 
         # Detect apriltags in the image
-        results = self.detector.detect(gray)
-        
-        frame = cv2.cvtColor(gray, cv2.COLOR_GRAY2BGR)
+        results = self.detector.detect(sharpened_image)
+        vis = cv2.cvtColor(sharpened_image, cv2.COLOR_GRAY2BGR)
+
 
         # Loop through the detected apriltags
         for r in results:
@@ -63,24 +68,33 @@ class ApriltagCtrl(Node):
             corners = r['lb-rb-rt-lt'].astype(int)
         
             # Draw a polygon around the apriltag
-            cv2.polylines(frame, [corners], isClosed=True, color=(0, 255, 0), thickness=2)
+            cv2.polylines(vis, [corners], isClosed=True, color=(0, 255, 0), thickness=2)
         
             # Get the center of the apriltag
             center_x, center_y = int(r['center'][0]), int(r['center'][1])
+            self.get_logger().info(f"{center_x, center_y}")
             # Draw a circle at the center of the apriltag
-            cv2.circle(frame, (center_x, center_y), 5, (0, 0, 255), -1)
-        
+            cv2.circle(vis, (center_x, center_y), 5, (0, 0, 255), -1)
+
             # Print the ID and center of the apriltag
             print(f'Tag ID: {r["id"]}, Center: ({center_x}, {center_y})')
 
         # Convert the OpenCV image back to a ROS Image message
-        frame = cv2.resize(frame, (frame.shape[1] // scale, frame.shape[0] // scale))
-        result_img_msg = self.bridge.cv2_to_imgmsg(frame, encoding="bgr8")
-        # result_img_msg = self.bridge.cv2_to_imgmsg(frame)
+        result_img_msg = self.bridge.cv2_to_imgmsg(vis, encoding="bgr8")                                                                                      
         # Publish the result image message
         self.apriltag_ctrl_publisher.publish(result_img_msg)
         # Show the result image
-        cv2.imshow('ctrled Image', frame)
+
+
+        width = int(frame.shape[1])
+        height = int(frame.shape[0])
+        dim = (width, height)
+        
+        # resize image
+        resized2 = cv2.resize(vis, dim, interpolation = cv2.INTER_AREA)
+
+
+        cv2.imshow('ctrled Image', resized2)
         # Wait for 1 millisecond
         cv2.waitKey(1)
 
