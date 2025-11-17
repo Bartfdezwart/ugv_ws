222275e6e3db2a07d9c0ef877f2375ada38244e4
Rick van der Veen <rickvdv104@gmail.com>
2025-11-17 17:24:57 +0100

    Use single resolve_ros_package function

--- src/ugv_main/ugv_tools/ugv_tools/urdf_loader.py
+++ src/ugv_main/ugv_tools/ugv_tools/urdf_loader.py
@@ -2,56 +2,16 @@
 import contextlib
 import io
 from pathlib import Path
-from typing import Optional
 
 import numpy as np
+import rerun as rr
 import scipy.spatial.transform as st
 import trimesh
 import trimesh.visual
 from PIL import Image
 from urdf_parser_py import urdf as urdf_parser
 
-import rerun as rr
-
-
-def resolve_ros_path(path_str: str) -> str:
-    """Resolve a ROS path to an absolute path."""
-    if path_str.startswith("package://"):
-        path = Path(path_str)
-        package_name = path.parts[1]
-        relative_path = Path(*path.parts[2:])
-
-        package_path = resolve_ros1_package(package_name) or resolve_ros2_package(
-            package_name
-        )
-
-        if package_path is None:
-            raise ValueError(
-                f"Could not resolve {path}."
-                f"Replace with relative / absolute path, source the correct ROS environment, or install {package_name}."
-            )
-
-        return str(package_path / relative_path)
-    elif path_str.startswith("file://"):
-        return path_str[len("file://") :]
-    else:
-        return path_str
-
-
-def resolve_ros2_package(package_name: str) -> Optional[str]:
-    try:
-        import ament_index_python
-
-        try:
-            return ament_index_python.get_package_share_directory(package_name)
-        except ament_index_python.packages.PackageNotFoundError:
-            return None
-    except ImportError:
-        return None
-
-
-def resolve_ros1_package(package_name: str) -> Optional[str]:
-    return None
+from ugv_tools.utils import resolve_ros_path
 
 
 class URDFLogger:
